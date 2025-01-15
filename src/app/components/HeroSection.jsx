0d2311c48d7b9e87b322f7bf77372f67452c1b7d
FEELTHEVENOM <rahul.valli2003@gmail.com>
2025-01-15 16:02:19 +0530

    fixed profile image

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -88,7 +88,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#f6bd60] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/profile.png"
+              src="./images/profile.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={400}
